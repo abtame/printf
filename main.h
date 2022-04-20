5f9c9ebe31c3f55514c9086e63013e8a05b8fa97
root <you@exampl.com>
2022-04-20 11:59:34 -0700

    task update

--- main.h
+++ main.h
@@ -1,41 +1,45 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#define SIZE 2048
+
+#include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
 int _printf(const char *format, ...);
-int print_char(va_list ap);
-int print_str(va_list ap);
-int print_nbr(va_list ap);
-int print_binary(va_list ap);
-int print_octal(va_list ap);
-int print_hexa_lower(va_list ap);
-int print_hexa_upper(va_list ap);
-int print_unsigned(va_list ap);
-int print_str_unprintable(va_list ap);
-int print_str_reverse(va_list ap);
-int print_ptr(va_list ap);
-int print_rot13(va_list ap);
-int print_percent(va_list ap __attribute__((unused)));
-int print_number(int n);
-int print_unsigned_number(unsigned int n);
-int _putchar(char c);
-int _puts(char *str, int ascii);
-int _nbr_len(int prmNumber);
-int _strlen_recursion(char *s);
-int convert_alpha_numeric(int nb, int upper);
-char *convert_rot13(char *str);
-char *convert_base(unsigned long nb, unsigned int base, int upper);
-char *_strdup(char *str);
-char *convert_base_pointer(unsigned long p);
+int (*format_type(char *s))(va_list vlist, char *output_p, int o_p);
+int print_percent(va_list vlist, char *output_p, int o_p);
+int print_char(va_list vlist, char *output_p, int o_p);
+int s_trlen(char *str);
+int print_string(va_list vlist, char *output_p, int o_p);
+int print_numbers(int n, char *output_p, int o_p);
+int print_int(va_list vlist, char *output_p, int o_p);
+int print_single_int(va_list vlist, char *output_p, int o_p);
+int print_octal(va_list vlist, char *output_p, int o_p);
+int print_unumber(va_list vlist, char *output_p, int o_p);
+int print_hexa(va_list vlist, char *output_p, int o_p);
+int print_HEXA(va_list vlist, char *output_o, int o_p);
+int print_HEXA_S(char s, char *output_p, int o_p);
+int print_S_string(va_list vlist, char *output_p, int o_p);
+int print_paddress(va_list vlist, char *output_p, int o_p);
+int print_rot13(va_list vlist, char *output_p, int o_p);
 
 /**
- * struct flags_printf - struct conversion to function
- * @c: flag string
- * @f: pointer to func
+ * struct t_format - Struct t_format
+ *
+ * @character: the class
+ * @func: pointer to a function
+ *
+ * Description: the structure for the format type
  */
-
-typedef struct flags_printf
+typedef struct t_format
 {
-	char *c;
-	int (*f)(va_list);
-} flags_p;
+	char *character;
+	int (*func)(va_list vlist, char *output_p, int o_p);
+} t_f;
+
 #endif
